492b08acbd825964bf4a6943f2015c68f876bd9e
falsy <me@falsy.me>
2017-02-26 16:01:55 +0900

    스티키 네비게이션 수정

--- falsy.js
+++ falsy.js
@@ -4,7 +4,12 @@ jQuery(document).ready(function($) {
 	//scroll
 	$(window).scroll(function() {
 		if($(window).scrollTop() < 2000) {
-			$('.visual').css('background-position', '50% ' + ($(window).scrollTop()/-5) + 'px');
+			$('#visual').css('background-position', '50% ' + ($(window).scrollTop()/-5) + 'px');
+		}
+		if($(window).scrollTop() > ($(window).height()/2)) {
+			$('#sub-header').addClass('show');
+		} else {
+			$('#sub-header').removeClass('show');
 		}
 	});
 
