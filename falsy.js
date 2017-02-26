75072a727ef77b12148bb2370f60597d7008b273
falsy <me@falsy.me>
2017-02-26 19:45:00 +0900

    스크립트 정리

--- falsy.js
+++ falsy.js
@@ -13,15 +13,17 @@ jQuery(document).ready(function($) {
 		}
 	});
 
+
+	//click
 	$('.page-navigation a').on('click', function() {
 		event.preventDefault();
 
-		var $targetEl = $(this).attr('href');
+		var $targetEl = $($(this).attr('href'));
 		$('#content > section').removeClass('show');
-		$($targetEl).addClass('show');
+		$targetEl.addClass('show');
 
 		setTimeout(function(){
-			var targetPositionTop = $($targetEl).offset().top - 60;
+			var targetPositionTop = $targetEl.offset().top - 60;
 			$('html, body').animate({
 				scrollTop: targetPositionTop
 			}, 400);
@@ -32,13 +34,13 @@ jQuery(document).ready(function($) {
 	$('#essay a').on('click', function(){
 		event.preventDefault();
 
-		var $targetEl = $(this).attr('href');
+		var $targetEl = $($(this).attr('href'));
 		$('#essay-text > section').removeClass('show');
-		$($targetEl).addClass('show');
-		$($targetEl).css('min-height', $(window).height() - 60);
+		$targetEl.addClass('show');
+		$targetEl.css('min-height', $(window).height() - 60);
 
 		setTimeout(function(){
-			var targetPositionTop = $($targetEl).offset().top - 60;
+			var targetPositionTop = $targetEl.offset().top - 60;
 			$('html, body').animate({
 				scrollTop: targetPositionTop
 			}, 400);
@@ -47,12 +49,12 @@ jQuery(document).ready(function($) {
 
 	//retina
 	if(window.devicePixelRatio > 1) {
-        var $images = $('img');
+    var $images = $('img');
 
-        $images.each(function(i) {
-            var src = $(this).attr('src');
-            var retinaSrc = src.replace(".png", "X2.png");
-            $(this).attr('src', retinaSrc);
-        });
-    }
+    $images.each(function(i) {
+      var src = $(this).attr('src');
+      var retinaSrc = src.replace(".png", "X2.png");
+      $(this).attr('src', retinaSrc);
+    });
+  }
 });
